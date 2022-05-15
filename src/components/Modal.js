4510720db8a663ab7385d0a89ec9087f5290b4e3
Thomas McNish <mcnishthomas@gmail.com>
2022-05-15 10:15:32 -0400

    added modal window to test portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class Modal extends Component {
+class Modal extends Component {
   render() {
     return (
-
+      ReactDOM.createPortal(
+        this.props.children,
+        document.getElementById('my-portal')
+      )
     );
   }
 }
